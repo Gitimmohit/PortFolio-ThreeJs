382dd312b833908643ea6bed1b04c23860c308de
Mohit <immohitmahato@gmail.com>
2024-05-29 09:06:00 +0530

    email js and fox model added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,71 +1,69 @@
-import React, { useState } from 'react'
-import { useRef } from 'react'
-import emailjs from "@emailjs/browser"
+import React, { Suspense, useState } from "react";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { Canvas } from "@react-three/fiber";
+import Fox from "../models/Fox";
+import Loader from "../components/Loader"
+
 
 const Contact = () => {
-  const formRef = useRef(null)
+  const formRef = useRef(null);
 
-  // all the states 
-  const [form, setform] = useState({ name: "", email: "", message: "" })
-  const [isLoading, setisLoading] = useState(false)
-
-
-
+  // all the states
+  const [form, setform] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setisLoading] = useState(false);
 
   const handleChange = (e) => {
-    setform({ ...form, [e.target.name]: e.target.value })
-  }
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
 
-  const handleFocus = () => {
+  const handleFocus = () => {};
 
-  }
-
-  const handleBlur = () => {
-
-  }
+  const handleBlur = () => {};
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setisLoading(true)
+    setisLoading(true);
 
-    // for sending the email 
-    emailjs.send(
-      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-      {
-        from_name: form.name,
-        to_name: 'Mohit',
-        from_email: form.email,
-        to_email: 'mohitetechcube@gmail.com',
-        message: form.message,
-      }, import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY)
+    // for sending the email
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Mohit",
+          from_email: form.email,
+          to_email: "mohitetechcube@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
       .then(() => {
-      setisLoading(false);
-
-      setform({ name: '', email: '', message: '' })
-    }).catch((error) => {
-      setisLoading(false)
-      console.log(error)
-    })
-  }
-
+        setisLoading(false);
+        setform({ name: "", email: "", message: "" });
+      })
+      .catch((error) => {
+        setisLoading(false);
+        console.log(error);
+      });
+  };
 
   return (
-    <section className='relative flex lg:flex-row flex-col max-container'>
-      <div className='flex-1 min-w-[50%] flex flex-col'>
-        <h1 className='head-text'>Get In Touch</h1>
-        <form className='w-full flex flex-col gap-7 mt-14'
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get In Touch</h1>
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
           onSubmit={handleSubmit}
         >
-
-
-          <label className='text-black-500 font-semibold'>
+          <label className="text-black-500 font-semibold">
             Name
             <input
-              type='text'
-              name='name'
-              className='input'
-              placeholder='mohit kumar'
+              type="text"
+              name="name"
+              className="input"
+              placeholder="mohit kumar"
               required
               value={form.name}
               onChange={handleChange}
@@ -74,13 +72,13 @@ const Contact = () => {
             />
           </label>
 
-          <label className='text-black-500 font-semibold'>
+          <label className="text-black-500 font-semibold">
             Email
             <input
-              type='email'
-              name='email'
-              className='input'
-              placeholder='mohit@gmail.com'
+              type="email"
+              name="email"
+              className="input"
+              placeholder="mohit@gmail.com"
               required
               value={form.email}
               onChange={handleChange}
@@ -89,16 +87,14 @@ const Contact = () => {
             />
           </label>
 
-
-
-          <label className='text-black-500 font-semibold'>
+          <label className="text-black-500 font-semibold">
             Your Message
             <input
-              type='text'
-              name='message'
+              type="text"
+              name="message"
               rows={4}
-              className='input'
-              placeholder='Let Me know how i can help you!'
+              className="input"
+              placeholder="Let Me know how i can help you!"
               required
               value={form.message}
               onChange={handleChange}
@@ -108,20 +104,38 @@ const Contact = () => {
           </label>
 
           <button
-            type='submit'
-            className='btn'
+            type="submit"
+            className="btn"
             onFocus={handleFocus}
             onBlur={handleBlur}
             disabled={isLoading}
           >
-            {isLoading ? "Sending...." : 'Send Message'}
+            {isLoading ? "Sending...." : "Send Message"}
           </button>
-
         </form>
-
+      </div>
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov:75,
+            near:0.1,
+            far:1000
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0,0,1]}/>
+          <ambientLight intensity={0.5}/>
+          <Suspense fallback={<Loader />}>
+            <Fox 
+            position={[0.5,0.35,0]}
+            rotation={[12.6,-0.6,0]}
+            scale ={[0.5,0.5,0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
